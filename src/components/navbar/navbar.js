a4f6ae48c79fe115c3d33afd04335760030a751d
Ivar Oli <ivars17@ru.is>
2019-09-14 14:43:20 +0000

    navbar prototype

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,42 +5,40 @@ class Navbar extends Component {
   render() {
     return (
       <nav className={style.reboot_navbar + " navbar navbar-expand-lg navbar-dark bg-dark"}>
-        <div class="d-flex flex-grow-1">
-            <span class="w-100 d-lg-none d-block"></span>
-            <a class="navbar-brand" href="#">
+        <div className="d-flex flex-grow-1">
+            <span className="w-100 d-lg-none d-block"></span>
+            <a className="navbar-brand" href="#">
                 <img src="/images/reboothack_small.png" alt="The logo for the website" height="50px"></img>
                 <img src="/images/reboothack_default_navbar.png" height="50px"></img>
             </a>
-            <div class="w-100 text-right">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
-                    <span class="navbar-toggler-icon"></span>
+            <div className="w-100 text-right">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
         </div>
-        <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
-            <ul class="navbar-nav ml-auto flex-nowrap">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">FaQ</a>
+        <div className="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
+            <ul className="navbar-nav ml-auto flex-nowrap">
+                <li className={style.reboot_navbar_item + " nav-item"}>
+                    <a href="#" className="nav-link">FaQ</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">About</a>
+                <li className={style.reboot_navbar_item + " nav-item"}>
+                    <a href="#" className="nav-link">About</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div class="dropdown-menu pull-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                <li className={style.reboot_navbar_item + " nav-item"}>
+                <div className="btn-group">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Team</a>
+                  <div className="dropdown-menu pull-right" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#">Board Of Advisors</a>
+                    <a className="dropdown-item" href="#">Team Members</a>
                   </div>
+                </div>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Sponsors</a>
+                <li className={style.reboot_navbar_item + " nav-item"}>
+                    <a href="#" className="nav-link">Sponsors</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">TUNGUMAAAL!!</a>
+                <li className={style.reboot_navbar_language + " nav-item"}>
+                    <a href="#" className="nav-link">IS <img src="/images/icelandic.png" height="15px"></img></a>
                 </li>
             </ul>
         </div>
