dba2632415525b5172e2f6abfcffa24e628336f9
Ívar Óli <ivars17@ru.is>
2019-09-20 23:31:13 +0000

    translation system complete

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Navbar from '../../components/navbar';
+import {changeLanguage} from '../../translations';
 import style from "./index.css"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handler = this.handler.bind(this)
+  }
+
+  handler() {
+    console.log("asdf");
+    changeLanguage();
+    this.forceUpdate();
+  }
+  
   render() {
     return (
       <div className={style.App}>
-        <Navbar />
+        <Navbar handler = {this.handler}/>
         <h1>HALLO!!!</h1>
 
       </div>
---
+++ src/text.config.js
@@ -0,0 +1,22 @@
+export const translation = {
+    is: {
+      navbar:{
+        team: "Teymi",
+        about: "Um Okkur",
+        sponsors: "Bakhjarlar",
+        trans_btn: "EN <img src='/images/english.png' height='15px'></img>",
+        board: "Ráðgjafar",
+        team_members: "Reboot Team Teymið"
+      }
+    },
+    en:{
+      navbar:{
+        team: "Team",
+        about: "About Us",
+        sponsors: "Sponsors",
+        trans_btn: "IS <img src='/images/icelandic.png' height='15px'></img>",
+        board: "Board Of Advisors",
+        team_members: "The Reboot Team"
+      }
+    }
+};
