196ba470db58e58b3eebdd13fa206e2a81dade1a
George Morosanu <georgecosminmorosanu@gmail.com>
2019-11-21 22:18:51 +0000

    Small Details - fixed

--- src/components/Teams/Team/Team.js
+++ src/components/Teams/Team/Team.js
@@ -21,6 +21,7 @@ class Team extends Component {
       });
     return (
       <div className={TeamStyle.Container+" container"}>
+        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */}
         <div className={TeamStyle.TeamDiv}>
           {/* <p className={TeamStyle.TeamName}> {this.props.teamName} {this.props.teamName=='Director' ? null : 'Team'}</p> */}
           
--- src/components/applyButton/applyButton.js
+++ src/components/applyButton/applyButton.js
@@ -31,18 +31,18 @@ class ApplyButton extends Component {
             <div className="container">
 
                 <div className="text-center">
-                    <button type="button" className={style.apply_button + "btn btn-outline-primary btn-lg mb-4"} data-toggle="modal" data-target="#modalRegisterForm">
+                    <button type="button" className={"btn btn-outline-primary btn-lg mb-4 "+style.apply_button} data-toggle="modal" data-target="#modalRegisterForm">
                         {this.text.register.subHeading}
                     </button>
                 </div>
 
                 <div className="modal fade" id="modalRegisterForm" tabIndex="-1" role="dialog" >
                     <div className="modal-dialog" role="document">
-                        <div className="modal-content mx-3">
+                        <div className={"modal-content mx-3 " +style.modal_content}>
                             {/* header */}
                             <div className="modal-header">
-                                <button type="button" className="close" data-dismiss="modal">&times;</button>
                                 <h3 className="modal-title">{this.text.register.registerDialog.title}</h3>
+                                <button type="button" className={"close "+style.close } data-dismiss="modal">&times;</button>
                             </div>
                             {/* body */}
                             <div className="modal-body">
@@ -53,7 +53,7 @@ class ApplyButton extends Component {
                                     {/* Name */}
                                     <div className="form-group input-group">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text"> <i className="fa fa-user"></i> </span>
+                                            <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-user "+style.iconMarginLeft}></i> </span>
                                         </div>
                                         <input id="reg_name" className="form-control" placeholder={this.text.register.registerDialog.name} type="text" required></input>
                                     </div>
@@ -61,7 +61,7 @@ class ApplyButton extends Component {
                                     {/* Email */}
                                     <div className="form-group input-group">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
+                                            <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-envelope "+style.iconMarginLeft}></i> </span>
                                         </div>
                                         <input id="reg_email" type="email" className="form-control" placeholder={this.text.register.registerDialog.email} required></input>
                                     </div>
@@ -69,7 +69,7 @@ class ApplyButton extends Component {
                                     {/* School */}
                                     <div className="form-group input-group">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text"> <i className="fa fa-building"></i> </span>
+                                            <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-building "+style.iconMarginLeft}></i> </span>
                                         </div>
                                         <input id="reg_school" className="form-control" placeholder={this.text.register.registerDialog.school} type="text" required></input>
                                     </div>
@@ -77,7 +77,7 @@ class ApplyButton extends Component {
                                     {/* Study */}
                                     <div className="form-group input-group">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text"> <i className="fa fa-graduation-cap"></i> </span>
+                                            <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-graduation-cap "+style.iconMarginLeft}></i> </span>
                                         </div>
                                         <input id="reg_study" className="form-control" placeholder={this.text.register.registerDialog.study} type="text" required></input>
                                     </div>
@@ -85,7 +85,7 @@ class ApplyButton extends Component {
                                     {/* T-shirt Size */}
                                     <div className="form-group input-group">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text"> <i className="fa fa-smile"></i> </span>
+                                            <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-smile-o "+style.iconMarginLeft}></i> </span>
                                         </div>
                                         <select id="reg_tShirt" className="form-control" placeholder={this.text.register.registerDialog.study} type="text" required>
                                             <option>{this.text.register.registerDialog.tShirt}</option>
@@ -102,7 +102,7 @@ class ApplyButton extends Component {
                                      {/* Better to have NOT required, what if you don't have any allergies? */}
                                      <div className="form-group input-group">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text"> <i className="fa fa-cutlery"></i> </span>
+                                            <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-cutlery "+style.iconMarginLeft}></i> </span>
                                         </div>
                                         <input id="reg_food" className="form-control" placeholder={this.text.register.registerDialog.diet} type="text"></input>
                                     </div>
@@ -111,13 +111,13 @@ class ApplyButton extends Component {
                                     <div className="form-group input-group-checkbox">
                                         <div className="input-group">
                                             <label className="form-check form-check-inline">
-                                                <span className="input-group-text"> <i className="fa fa-users"></i> </span>
+                                                <span className={"input-group-text "+ style.inputGroupTextStyle}> <i className={"fa fa-users "+style.iconMarginLeft}></i> </span>
                                             </label>
                                             <label className="form-check form-check-inline">
                                                 <input id="reg_team" className="form-check-input" type="checkbox"></input>
                                             </label>
                                             <label className="form-check form-check-inline">
-                                                <label className="form-control-label" >{this.text.register.registerDialog.team}</label>
+                                                <label className={"form-control-label "+style.labelAligned} >{this.text.register.registerDialog.team}</label>
                                             </label>
                                         </div>
                                     </div>
@@ -134,7 +134,7 @@ class ApplyButton extends Component {
                             {/* footer */}
                             {/* redirects to conformation site.. Just for conformation!  */}
                             <div className="modal-footer modal-guts">
-                                <button className="btn btn-primary btn-block" type="submit" onClick={this.handleClick}>{this.text.register.registerDialog.registerBtn}</button>
+                                <button className={"btn btn-primary btn-block " +style.register_button} type="submit" onClick={this.handleClick}>{this.text.register.registerDialog.registerBtn}</button>
                             </div>                            
 
                         </div>
