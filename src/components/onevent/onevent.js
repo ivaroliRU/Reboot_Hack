77513fd05875fd4d0f78f6cd1036c7a7fb531abd
einargudnig <einargudnig@gmail.com>
2020-02-04 11:48:14 +0000

    twitter + new photos

--- src/components/onevent/onevent.js
+++ src/components/onevent/onevent.js
@@ -6,6 +6,9 @@ import Challenges from './challenges/challenges';
 import Location from './location/location';
 import Events from './events/events'
 import InstaGrid from './judges/judges';
+import Twitter from './twitter/twitter';
+
+
 
 class OnEvent extends Component {
   
@@ -21,18 +24,25 @@ class OnEvent extends Component {
         <div className={"container"}>
           <br />
           {/*<h1 className={style.text_styling+" "+style.text_align}><b>Reboot Hack 2020</b></h1>*/}
+          
+          <div className={"row pb-3"}>
+            <div className={"col-12"}>
+              <Twitter />
+            </div>
+          </div>
+
+          <div className={"row pb-3"}>
+            <div className={"col-12"}>
+              <Schedule />
+            </div>
+          </div>
+
           <div className={"row"}>
             <div className={"col-lg-8 col-sm-12"}>
               <Challenges /> 
             </div>
             <div className={"col-lg-4 col-sm-12"}>
               <Events />
-            </div>
-          </div>
-          
-          <div className={"row pb-3"}>
-            <div className={"col-12"}>
-              <Schedule />
             </div>
           </div>
 
---
+++ src/components/onevent/twitter/twitter.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
+class Twitter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <TwitterTimelineEmbed
+                    sourceType = 'profile'
+                    screenName = 'einargudni'
+                    options = {{height: 400}}
+                />
+
+                <div className="d-flex justify-content-center">
+                    
+                    <div className="pl-3 pr-3">
+                        <TwitterFollowButton
+                            screenName = {'einargudni'}
+                        />
+                    </div>
+
+                    <div className="pl-3 pr-3">
+                        <TwitterHashtagButton
+                            tag = {'reboothack2020'}
+                        />
+                    </div>
+
+                    <div className="pl-3 pr-3">
+                        <TwitterMentionButton
+                            screenName = {'einargudni'}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Twitter;
