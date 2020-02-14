a780b3485f54db4e7e47dac3b3c19a1da672855b
einargudnig <einargudnig@gmail.com>
2020-02-14 15:12:10 +0000

    tiny fix on vordur challenge

---
+++ src/components/onevent/twitter/twitter.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
+class Twitter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <TwitterTimelineEmbed
+                    sourceType = 'profile'
+                    screenName = 'reboothack'
+                    options = {{height: 600}}
+                />
+
+                <div className="d-flex justify-content-center">
+                    
+                    <div className="pl-3 pr-3">
+                        <TwitterFollowButton
+                            screenName = {'Reboothack'}
+                        />
+                    </div>
+
+                    <div className="pl-3 pr-3">
+                        <TwitterHashtagButton
+                            tag = {'reboothack2020'}
+                        />
+                    </div>
+
+                    <div className="pl-3 pr-3">
+                        <TwitterMentionButton
+                            screenName = {'Reboothack'}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Twitter;
