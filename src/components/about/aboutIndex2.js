62379a53bf66f6f9871a10cf473960b91b48d6fe
einargudnig <einargudnig@gmail.com>
2020-04-30 16:55:53 +0000

    fix link

--- src/components/about/aboutIndex2.js
+++ src/components/about/aboutIndex2.js
@@ -12,7 +12,7 @@ class AboutIndex2 extends Component {
                         <p>{this.text.detail.indexThird.orgText}</p>                        
                         <div className="justify-content-md-center text-center">
 
-                            <a type="" className={"btn btn-outline-primary btn-lg mb-4 "+ style.apply_button} id="volunteer" href="https://docs.google.com/forms/d/e/1FAIpQLSck8aWhP4VCoZMbn6D8DgrC5tq1vm1bmiySQ91R0ygo8Wh6ZA/viewform" target="_blank">
+                            <a type="" className={"btn btn-outline-primary btn-lg mb-4 "+ style.apply_button} id="volunteer" href="https://forms.gle/vFmS2RKpDcoyXXu86" target="_blank">
                                 {this.text.detail.indexThird.button}
                             </a>
                         </div>
